118f9c331156a7036d0b930de3b9d6a1b2164c01
Rainner Guimaraes <reidnao@gmail.com>
2023-04-05 22:55:13 -0300

    Create pastas

---
+++ Enrollment.Management.Registration.Api/Configurations/DependencyInjection.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Enrollment.Management.Registration.Api.Configurations
+{
+    public static class DependencyInjection
+    {
+        public static void AddInfrastructureApi(this IServiceCollection services, IConfiguration configuration)
+        {
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+            //services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
+            //services.AddScoped<IProductRepository, ProductRepository>();
+            //services.AddScoped<IProductService, ProductService>();
+
+            //services.AddAutoMapper(typeof(DomainToDtoMappingProfile));
+        }
+    }
+}
